cb76ae8eda996caa69b6fa196601a8b2019e85cd
Yas Wood <yaswood@Yass-MacBook-Air.local>
2022-07-26 10:41:06 -0400

    done

--- src/main/java/cd/get/ready/algorithms/CountDuplicates.java
+++ src/main/java/cd/get/ready/algorithms/CountDuplicates.java
@@ -1,5 +1,10 @@
 package cd.get.ready.algorithms;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class CountDuplicates {
     /**
      * Example of overloading a function that accepts a Array String
@@ -7,7 +12,17 @@ public class CountDuplicates {
      * @return
      */
     public Integer countDuplicates(String[] input){
-        return null;
+        Map<String, Integer> map = new HashMap<String,Integer>();
+        Set<String> set = new TreeSet<String>();
+        for (String string:input) {
+            map.merge(string,1,Integer::sum);
+            if(map.get(string) > 1){
+                set.add(string);
+            }
+
+        }
+        return set.size();
+
     }
 
     /**
@@ -16,6 +31,15 @@ public class CountDuplicates {
      * @return
      */
     public Integer countDuplicates(Integer[] input){
-        return null;
+        Map<Integer, Integer> map = new HashMap<Integer,Integer>();
+        Set<Integer> set = new TreeSet<Integer>();
+        for (Integer ints:input) {
+            map.merge(ints,1,Integer::sum);
+            if(map.get(ints) > 1){
+                set.add(ints);
+            }
+
+        }
+        return set.size();
     }
 }
